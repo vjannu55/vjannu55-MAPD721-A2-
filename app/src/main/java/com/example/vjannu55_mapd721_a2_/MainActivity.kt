5772fcd5b448a9f6b3da6d15394b6cb067fcb8b5
vjannu55 <146692732+vjannu55@users.noreply.github.com>
2024-02-12 21:30:46 +0500

    vjannu55-MAPD721-A2-

---
+++ app/src/main/java/com/example/vjannu55_mapd721_a2_/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vjannu55_mapd721_a2_
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
