1d7617fdaddb989b1a2449679e99ef1ef74daf40
vjannu55 <146692732+vjannu55@users.noreply.github.com>
2024-02-12 21:49:53 +0500

    vjannu55-MAPD721-A2-

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "vjannu55-MAPD721-A2-"
+include(":app")
+ 
